200215612385932955195e7ec418d196a8ed7945
evanJack10 <jackson4eb@gmail.com>
2026-07-15 16:05:43 -0400

    Changes to the Correlator Code (Still rough draft)

--- rootScripts/Correlator.C
+++ rootScripts/Correlator.C
@@ -10,6 +10,7 @@ void SwitchRowCol (int rc1, int rc2) {
     TMatrixT<double>* mCij = (TMatrixT<double>*)gDirectory->Get("C_ij");
     TMatrixT<double>* mRij = (TMatrixT<double>*)gDirectory->Get("R_ij");
     TMatrixT<double>* mVij = (TMatrixT<double>*)gDirectory->Get("V_ij");
+    std::cout << rc1 << " " << rc2 << std::endl;
     for (int col = 0; col < mCij->GetNcols(); col++) {
         double temp = (*mCij)(rc1, col);
         (*mCij)(rc1, col) = (*mCij)(rc2, col);
@@ -40,7 +41,6 @@ void SwitchRowCol (int rc1, int rc2) {
         (*mVij)(row, rc1) = (*mVij)(row, rc2);
         (*mVij)(row, rc2) = temp;
     }
-    std::cout << "mCij address (inside func): " << (void*)mCij << std::endl;
     mCij->Write("C_ij", TObject::kOverwrite);
     mRij->Write("R_ij", TObject::kOverwrite);
     mVij->Write("V_ij", TObject::kOverwrite);
@@ -84,29 +84,84 @@ void ChangeIVDV () {
         InputType.push_back(type); 
     }
 
-// This undoes the last row and column swaps done so you don't have to run the program twice to get the original matrix back
+    // This undoes the last row and column swaps done so you don't have to run the program twice to get the original matrix back
     if (inst.size() >= 1) {
         for (int i = 0; i < inst.size(); i += 2) {
-            int cr1 = inst[i]; int cr2 = inst[i + 1];
+            int cr1 = inst[inst.size() - i - 1]; int cr2 = inst[inst.size() - i - 2];
             SwitchRowCol(cr1, cr2);
         }
     }
     inst.clear();
 
-    // Makes sure both lists have the same variables in the same order
-    // I hope to keep working on this so that the list doesn't have to be in a specific order but just have the same variables
-    for (int i = 0; i < CombinedType.size(); i++) {
-        if (CombinedName[i] != InputName[i]) {
-            std::cout << CombinedName[i] << " is not the same as " << InputName[i] << std::endl;
+    //Making sure the the variable actually existed in the original matrix and that they have the correct type
+    for (int i = 0; i < InputName.size(); i++) {
+        bool locate = false;
+        for (int j = 0; j < CombinedName.size(); j++) {
+            if (InputName[i] == CombinedName[j]) {
+                locate = true;
+            }
+        }
+        if (InputType[i] != "dv" && InputType[i] != "iv" && InputType[i] != "rm") {
+            std::cout << InputType[i] << " is an unknown type. Please use either dv, iv, or rm. Aborting Program" << std::endl;
+            return;
+        }
+        if (locate == false) {
+            std::cout << InputType[i] << "/" << InputName[i] << " is not listed in the original matrix. Aborting program" << std::endl;
+            return;
+        }
+    }
+    std::vector<int> RemoveVar;
+    std::vector<std::string> IndepVar;
+
+    // This checks the input file and tells us what we are removing and changing in the original matrix 
+    //also it edits the CombinedType so we can work independent of the Input file
+    for (int i =0; i < CombinedName.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < InputName.size(); j++) {
+            if (CombinedName[i] == InputName[j]) {
+                found = true;
+                if (CombinedType[i] == InputType[j]) {
+                    continue;
+                }
+                else {
+                    std::cout << "Changing " << CombinedName[i] << " from " << CombinedType[i] << " to " << InputType[j] << std::endl;
+                    CombinedType[i] = InputType[j];
+                }
+                if (InputType[j] == "rm") {
+                    found = false;
+                }
+            }
+        }
+        if (!found) {
+            std::cout << "Removing " << CombinedName[i] << " from original matrix" << std::endl;
+            RemoveVar.push_back(i);
+        }
+    }
+
+    //This removes variables that do not appear in the input file but do appear in the original matrix or have rm as the type
+    //It doesn't actually remove anything but moves the unwanted variables on the bottom so we can filter them out in the actual solver code
+    //Note that we can remove from the original matrix but we can't add new variables
+    int t = CombinedName.size() - 1;
+    for (int i = 0; i < RemoveVar.size(); i++) {
+        int rc1 = t;
+        int rc2 = RemoveVar[i];
+        std::string tempType = CombinedType[rc2];
+        CombinedType[rc2] = CombinedType[rc1];
+        CombinedType[rc1] = tempType;
+        std::string tempName = CombinedName[rc2];
+        CombinedName[rc2] = CombinedName[rc1];
+        CombinedName[rc1] = tempName;
+        SwitchRowCol(rc1, rc2);
+        inst.push_back(rc1); inst.push_back(rc2);
+        t -= 1;
+    }
+
+    //IndepVar lists the # of independent variables so that the sorting loop knows how many row swaps to do
+    for (int i = 0; i < InputType.size(); i++) {
+        if (CombinedType[i] == "iv") {
+            IndepVar.push_back(CombinedName[i]);
         } }
     
-    //IndepVar lists the # of independent variables so that the sorting loop knows how many row swaps to do
-        std::vector<int> IndepVar;
-    for (int i = 0; i < InputType.size(); i++) {
-        if (InputType[i] == "iv") {
-            IndepVar.push_back(i);
-        } }
-
     //defines # of dependent and independent variables I hope that this can change nY and nP in the actual solve funtion to define the submatrix cuts
     int nP = IndepVar.size();
     int nY = InputType.size() - IndepVar.size();
@@ -124,18 +179,18 @@ void ChangeIVDV () {
     for (int i = 0; i < IndepVar.size(); i++) {
         int lastI, earlyD, earlyI = -1;
         for (int k = 0; k < InputType.size(); k++) {
-            if (InputType[k] == "iv") {
+            if (CombinedType[k] == "iv") {
                 lastI = k;
             }
         }
         for (int h = j; h < InputType.size(); h++) {
-            if (InputType[h] == "iv") {
+            if (CombinedType[h] == "iv") {
                 earlyI = h;
                 break;
             }
         }
         for (int m = 0; m < InputType.size(); m++) {
-            if (InputType[m] == "dv") {
+            if (CombinedType[m] == "dv") {
                 earlyD = m;
                 break;
             }
@@ -146,13 +201,12 @@ void ChangeIVDV () {
         if (rc1 == rc2) {
             continue;
         }
-        std::cout << rc1 << " " << rc2 << std::endl;
-        std::string tempType = InputType[rc2];
-        InputType[rc2] = InputType[rc1];
-        InputType[rc1] = tempType;
-        std::string tempName = InputName[rc2];
-        InputName[rc2] = InputName[rc1];
-        InputName[rc1] = tempName;
+        std::string tempType = CombinedType[rc2];
+        CombinedType[rc2] = CombinedType[rc1];
+        CombinedType[rc1] = tempType;
+        std::string tempName = CombinedName[rc2];
+        CombinedName[rc2] = CombinedName[rc1];
+        CombinedName[rc1] = tempName;
         SwitchRowCol(rc1, rc2);
         inst.push_back(rc1); inst.push_back(rc2);
     }
